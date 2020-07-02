b1371f99ca894dd225ccaa6a5a4dba6d9708bce6
veeru153 <32760644+veeru153@users.noreply.github.com>
2020-07-03 00:33:18 +0530

    Added assets

---
+++ src/components/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import styles from './Home.module.css';
+
+const Home = () => {
+    return (
+        <div className={styles.Home}>
+            <div>
+                {/* Navbar */}
+            </div>
+            <div className="d-flex w-100 h-100">
+                <h1>LOOΠΔVERSE</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
